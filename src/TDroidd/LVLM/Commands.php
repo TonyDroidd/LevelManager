7717cd3e907f6e79a8bb23d99a2e741bb356a897
TonyDroidd <tonydroidd@gmail.com>
2016-01-28 07:13:00 -0700

    Hello World

--- src/TDroidd/LVLM/Commands.php
+++ src/TDroidd/LVLM/Commands.php
@@ -17,16 +17,18 @@ use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\command\ConsoleCommandSender;
 use pocketmine\utils\TextFormat;
+use pocketmine\level\Level;
 
-class Commands extends PluginBase implements CommandExecutor{
-    public function __construct(Main $plugin){
-        //parent::__construct("levelmanager", "Main command of LevelManager", "/lm gen:del levelName", ["lm", "lvm"]);
-        //$this->setPermission("levelmanager.command");
+class Commands extends PluginBase implements CommandExecutor
+{
+    public function __construct(Main $plugin)
+    {
         $this->plugin = $plugin;
     }
 
-    public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
-        if ($cmd->getName() === "levelmanager"){
+    public function onCommand(CommandSender $sender, Command $cmd, $label, array $args)
+    {
+        if ($cmd->getName() === "levelmanager") {
             if (isset($args[0])) {
                 switch (strtolower($args[0])) {
                     case "generate":
@@ -34,9 +36,8 @@ class Commands extends PluginBase implements CommandExecutor{
                     case "add":
                         if (isset($args[1])) {
                             $this->plugin->addLevel($args[1]);
-                            $this->plugin->getServer()->broadcastMessage("Debug");
                         } else {
-                            $this->getUsage();
+                            // $this->getUsage();
                             return true;
                         }
                         break;
@@ -45,19 +46,30 @@ class Commands extends PluginBase implements CommandExecutor{
                     case "delete":
                     case "del":
                         if (isset($args[1])) {
-                            $this->plugin->removeLevel($args[1], $sender);
-                            $this->plugin->getServer()->broadcastMessage("Debug");
+                            $this->plugin->removeLevel($args[1]);
                         } else {
-                            $this->getUsage();
+                            // $this->getUsage();
                             return true;
                         }
                         break;
+
+                    case "load":
+                        if (isset($args[1])) {
+                            $this->plugin->loadWorld(ucfirst($args[1]));
+                        } else {
+                            // $this->getUsage();
+                            return true;
+                        }
+                        break;
+                    case "unload":
+                        if (isset($args[1])) {
+                            $this->plugin->unloadWorld(ucfirst($args[1]));
+                        } else {
+                            // $this->getUsage();
+                            return true;
+                        }
                 }
-            } else {
-                $this->getUsage();
-                return true;
             }
         }
     }
 }
-
--- src/TDroidd/LVLM/Main.php
+++ src/TDroidd/LVLM/Main.php
@@ -19,14 +19,14 @@ use TDroidd\LVLM\Commands;
 use pocketmine\command\CommandMap;
 use pocketmine\command\CommandExecutor;
 
-class Main extends PluginBase implements Listener{
+class Main extends PluginBase implements Listener
+{
 
     public function onEnable()
     {
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
         $this->getLogger()->info(TextFormat::GREEN . "LevelManager Enabled!");
         $this->getCommand("levelmanager")->setExecutor(new Commands($this));
-        //$this->getServer()->getCommandMap()->register("levelmanager", new Commands($this));
     }
 
     public function addLevel($levelname)
@@ -36,15 +36,62 @@ class Main extends PluginBase implements Listener{
         return $this->getServer()->generateLevel($levelname);
     }
 
-    public function removeLevel($name, $sender)
-    {
+    public function removeLevel($name){
         $path = $this->getServer()->getDataPath() . "worlds/" . $name;
-        if (!file_exists($path)) {
-            return true;
+        $level = $this->getServer()->getLevelByName($name);
+        if ($level instanceof Level) {
+            $this->getServer()->unloadLevel($level);
         }
-        if (!is_dir($path)){
-            return unlink($path);
+        self::file_delDir($path);
+        $this->getServer()->broadcastMessage(TextFormat::GREEN . "World " . $name . " deleted!");
+    }
+
+    public function loadWorld($name){
+        if (!$this->getServer()->isLevelGenerated($name)) {
+            $this->getServer()->broadcastMessage(TextFormat::RED . "The level does not exist");
+            return false;
+        } elseif (!$this->getServer()->isLevelLoaded($name)){
+            $this->getServer()->broadcastMessage(TextFormat::YELLOW . "Level is not loaded. Loading...");
+            if ($this->getServer()->loadLevel($name)){
+                $this->getServer()->broadcastMessage(TextFormat::AQUA . "Level " . $name . " Loaded!");
+            }else{
+                $this->getServer()->broadcastMessage(TextFormat::RED . "The level couldn't be loaded");
+                return false;
+            }
+        }else{
+            $this->getServer()->broadcastMessage(TextFormat::AQUA . "Level is already loaded!");
+            return false;
         }
-        return rmdir($path);
+    }
+
+    public function unloadWorld($name){
+        $level = $this->getServer()->getLevelByName($name);
+        if(!$this->getServer()->isLevelGenerated($name)){
+            $this->getServer()->broadcastMessage(TextFormat::RED . "The level does not exist");
+            return false;
+        }
+        if (!$this->getServer()->isLevelLoaded($name)) {
+            $this->getServer()->broadcastMessage(TextFormat::YELLOW . "Level is not loaded.");
+            return false;
+        }
+        if ($level instanceof Level) {
+            $this->getServer()->unloadLevel($level);
+        }
+    }
+
+    public static function file_delDir($dir){
+        $dir = rtrim($dir, "/\\") . "/";
+        foreach (scandir($dir) as $file) {
+            if ($file === "." or $file === "..") {
+                continue;
+            }
+            $path = $dir . $file;
+            if (is_dir($path)) {
+                self::file_delDir($path);
+            } else {
+                unlink($path);
+            }
+        }
+        rmdir($dir);
     }
 }
